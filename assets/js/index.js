947aa401ad119b1a302604f30865c487ca777a7f
Mychol Zhou <Mychol@MBP-di-Mychol.home>
2023-04-17 20:44:26 +0200

    fixato chiusura dropdown al click dei link

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,11 @@ let item = document.querySelector('.icon-hamburger');
 item.addEventListener("click", function() {
   document.body.classList.toggle('header-menu-open');
 });
+
+let dropdownLinks = document.querySelectorAll('.header-menu a');
+
+dropdownLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    document.body.classList.remove('header-menu-open'); 
+  });
+});
